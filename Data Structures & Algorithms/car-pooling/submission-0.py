9258c709de472251442ad681bc549065da760aae
clevelandPaul <shengyang@uchicago.edu>
2026-05-06 12:04:39 -0500

    Add: car-pooling - submission-0

---
+++ Data Structures & Algorithms/car-pooling/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        # 第3个位置记录是start time: 1, 还是end time: 0
+        # end time需要排到start time前面
+        start_time = [[i, trips[i][1], 1] for i in range(len(trips))]
+        end_time = [[i, trips[i][2], 0] for i in range(len(trips))]
+        total_time = start_time + end_time
+        total_time.sort(key=lambda x: x[2])
+        total_time.sort(key=lambda x: x[1])
+
+        curr_people = 0
+        n = len(total_time)
+        print(total_time)
+        curr_time = 0
+        for i in range(n):
+            curr_time = total_time[i][1]
+            if total_time[i][2]==1: # start time
+                curr_people += trips[total_time[i][0]][0]
+                if curr_people>capacity:
+                    return False
+            else:
+                curr_people -= trips[total_time[i][0]][0]
+        return True
+
+
+        
