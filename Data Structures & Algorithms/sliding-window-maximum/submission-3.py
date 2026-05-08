7c3c57835562739ad32ec882d4ad14ed9c7bdad5
clevelandPaul <shengyang@uchicago.edu>
2026-05-08 17:00:28 -0500

    Add: sliding-window-maximum - submission-3

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-3.py
@@ -0,0 +1,17 @@
+from collections import deque
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        # 使用单调递减队列
+        # 那么队列中的头元素，就是窗口中的最大元素
+        queue = deque()
+        res = []
+        for i in range(len(nums)):
+            while queue and queue[0]<=i-k:
+                queue.popleft()
+            while queue and nums[queue[-1]]<=nums[i]:
+                queue.pop()
+            queue.append(i)
+            if i-k+1>=0:
+                res.append(nums[queue[0]])
+        return res
+              
